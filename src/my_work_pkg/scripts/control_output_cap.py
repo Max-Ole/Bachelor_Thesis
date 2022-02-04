b1325fdf8d61812ebf947c9f4e05051f6670428f
Max-Ole <maxolevonwaldow@outlook.de>
2022-02-04 19:51:07 +0100

    Fixed edge cases of derivitive in capping node. Capping node considers accel limit for braking. Rearranged code blocks.

--- src/my_work_pkg/scripts/control_output_cap.py
+++ src/my_work_pkg/scripts/control_output_cap.py
@@ -6,7 +6,7 @@ import rospy
 import rospkg
 import time
 from geometry_msgs.msg import Twist
-from math import copysign, sqrt, isnan
+from math import sqrt, isnan
 from copy import deepcopy
 from math import sqrt, tan, atan, asin, cos, degrees
 
@@ -41,19 +41,13 @@ class ControlCapClass():
 		We only regard the "manhatten"-velocity and acceleration, not the 2-norm, from the grid map as tipping is considered around the axes aka. sides of the robot """
 		command = deepcopy(msg)
 		
-		amax = (1-self.safetyMargin) * self.mapManager.getCellValue('a', *self.mapManager.pos2Cell(self.mapManager.mapPosition[1], self.mapManager.mapPosition[0]))
-		vmax = (1-self.safetyMargin) * self.maxVelFromCentripetalAccel(msg, amax) # 100
-		
-		# a = (v2-v1) / T_s  <=>  v2 = v1 + a*dt
-		dt = time.time() - self.last_t
-		vmax_from_amax = abs( self.last_v + amax*dt )
-		# need to satisfy both limits
-		v_limit_merged = float( min(vmax_from_amax, vmax) )
-		
-		if sum( [abs(msg.linear.x), abs(msg.linear.y), abs(msg.linear.z)] ) < 0.0001:
+		if sum( [abs(msg.linear.x), abs(msg.linear.y), abs(msg.linear.z)] ) < 0.01:
 			# velocity is zero, avoid division and
 			# pass Twist command unaltered
 			self.vel_pub.publish(msg)
+			
+			self.last_v = 0
+			self.last_t = time.time()
 			
 			# logging
 			self.logMotion( time.time(), \
@@ -74,13 +68,30 @@ class ControlCapClass():
 			return
 		
 		# velocity is non-zero
-		# w = v/r  <=>  r = v/w  <=>  v = w*r
-		# leave r as is, we do not want to change the robot's path. Also reduce velocity of all axes by the same factor
-		reduction_ratio = v_limit_merged / max(abs(msg.linear.x), abs(msg.linear.y), abs(msg.linear.z))
-		if reduction_ratio >= 1 or isnan(v_limit_merged):
-			# limits are not reached
+		amax = (1-self.safetyMargin) * self.mapManager.getCellValue('a', *self.mapManager.pos2Cell(self.mapManager.mapPosition[1], self.mapManager.mapPosition[0]))
+		if amax <= 0: 
+			# invalid limit, could be a sensor error
+			amax = float('nan') # alternative: halt robot
+		vmax = (1-self.safetyMargin) * self.maxVelFromCentripetalAccel(msg, amax) # 100
+		
+		# a = (v2-v1) / T_s  <=>  v2 = v1 + a*dt
+		dt = time.time() - self.last_t
+		vmax_from_amax = abs(self.last_v) + amax*dt
+		vmin_from_amax = max(0, abs(self.last_v) - amax*dt)
+		# need to satisfy both limits
+		v_limit_merged = float( min(vmax_from_amax, vmax) )
+
+		reduction_ratio = v_limit_merged / max(abs(msg.linear.x), abs(msg.linear.y), abs(msg.linear.z)) # upper bound
+		increase_ratio  = vmin_from_amax / max(abs(msg.linear.x), abs(msg.linear.y), abs(msg.linear.z)) # lower bound
+		
+		# take no action if: limit is nan  or  not up to date  or  msg is within limits
+		if isnan(v_limit_merged) or dt>=1 or (reduction_ratio >= 1 and increase_ratio <= 1) :
+			# msg is within limits
 			# pass Twist command unaltered
 			self.vel_pub.publish(msg)
+			
+			self.last_v = max(msg.linear.x, msg.linear.y, msg.linear.z)
+			self.last_t = time.time()
 			
 			# logging
 			self.logMotion( time.time(), \
@@ -99,54 +110,60 @@ class ControlCapClass():
 					msg.angular.z, \
 					)
 			return
-
 			
-		
-		assert reduction_ratio < 1, "ERROR: The reduction ratio ({}) when capping the control output should be smaller than 1".format(reduction_ratio)
-		# copysign(x,y) returns x with the sign of y.
-		#command.linear.x = copysign(min(v_limit_merged, abs(msg.linear.x)), msg.linear.x)
-		#command.linear.y = copysign(min(v_limit_merged, abs(msg.linear.y)), msg.linear.y)
-		#command.linear.z = copysign(min(v_limit_merged, abs(msg.linear.z)), msg.linear.z)
-		
-		# aply reduction ratio
-		# change angular velocity to keep radius the same. Ratio of reduction must be the same
-		command.linear.x  = reduction_ratio * msg.linear.x
-		command.linear.y  = reduction_ratio * msg.linear.x
-		command.linear.z  = reduction_ratio * msg.linear.x
-		command.angular.x = reduction_ratio * msg.angular.x
-		command.angular.y = reduction_ratio * msg.angular.y
-		command.angular.z = reduction_ratio * msg.angular.z
-		
-		# update states 
-		v_uncapped  = max(msg.linear.x, msg.linear.y, msg.linear.z)
-		v_current   = max(command.linear.x, command.linear.y, command.linear.z)
-		a_current   = abs(v_current - self.last_v) / dt
-		self.last_v = v_current
-		self.last_t = time.time()
-		
-		# print info # could change to 2norm for output: v_uncapped_2norm = sqrt(msg.linear.x**2 + msg.linear.y**2 + msg.linear.z**2)
-		print("capping acceleration: current accel: {}, cap: {}".format(a_current, amax))
-		print("capping velocity:     current vel  : {}, cap: {}".format(v_current, vmax))
-		
-		# publish capped control
-		self.vel_pub.publish(command)
-		
-		# logging
-		self.logMotion( time.time(), \
-						"capping", \
-						command.linear.x, \
-						command.linear.y, \
-						command.linear.z, \
-						command.angular.x, \
-						command.angular.y, \
-						command.angular.z, \
-						msg.linear.x, \
-						msg.linear.y, \
-						msg.linear.z, \
-						msg.angular.x, \
-						msg.angular.y, \
-						msg.angular.z, \
-						)
+		else:
+			if increase_ratio >= 1:
+				# lower limit exceeded
+				print( "lower limit exceeded" )
+				ratio = increase_ratio
+			elif reduction_ratio <= 1:
+				# upper limit exceeded
+				print( "upper limit exceeded" )
+				ratio = reduction_ratio
+			
+			# aply reduction ratio
+			# change angular velocity to keep radius r the same, we do not want to change the robot's path. Also reduce velocity of all axes by the same factor
+			# w = v/r  <=>  v = w*r  <=>  r = v/w = (v*ratio)/(w*ratio)
+			command.linear.x  = ratio * msg.linear.x
+			command.linear.y  = ratio * msg.linear.x
+			command.linear.z  = ratio * msg.linear.x
+			command.angular.x = ratio * msg.angular.x
+			command.angular.y = ratio * msg.angular.y
+			command.angular.z = ratio * msg.angular.z
+			
+			# update states 
+			v_uncapped  = max(msg.linear.x, msg.linear.y, msg.linear.z)
+			v_current   = max(command.linear.x, command.linear.y, command.linear.z)
+			a_uncapped  = (v_uncapped - self.last_v) / dt
+			a_current   = (v_current - self.last_v) / dt
+			
+			# print info # could change to 2norm for output: v_uncapped_2norm = sqrt(msg.linear.x**2 + msg.linear.y**2 + msg.linear.z**2)
+			print("\tchanging acceleration from {:6.2f} to {:6.2f}, cap: {:6.2f}".format(a_uncapped, a_current, amax))
+			print("\tchanging velocity (last:{:6.2f}) from {:6.2f} to {:6.2f}, between: {:6.2f} and {:6.2f}".format(v_uncapped, self.last_v, v_current, vmin_from_amax, v_limit_merged))
+			print("\tratio: {}, dt: {}".format(ratio, dt))
+			
+			self.last_v = v_current
+			self.last_t = time.time()
+			
+			# publish capped control
+			self.vel_pub.publish(command)
+			
+			# logging
+			self.logMotion( time.time(), \
+							"capping", \
+							command.linear.x, \
+							command.linear.y, \
+							command.linear.z, \
+							command.angular.x, \
+							command.angular.y, \
+							command.angular.z, \
+							msg.linear.x, \
+							msg.linear.y, \
+							msg.linear.z, \
+							msg.angular.x, \
+							msg.angular.y, \
+							msg.angular.z, \
+							)
 	
 	def maxVelFromCentripetalAccel(self, msg, amax):
 		""" Gets the max velocity from the maximal allowed acceleration and the current radius. From compile_robot_limitations.py:
